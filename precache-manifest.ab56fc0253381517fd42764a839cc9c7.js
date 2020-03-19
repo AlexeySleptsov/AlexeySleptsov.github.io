0a12998aec6223052a01b950219d9609f8a408a2
Alexey Sleptsov <kmb0637038557@gmail.com>
2020-03-19 16:40:25 +0300

    test task join to it

---
+++ precache-manifest.ab56fc0253381517fd42764a839cc9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21c997843e744033f0b2d832e1f193b",
+    "url": "/AlexeySleptsov.github.io/index.html"
+  },
+  {
+    "revision": "cd4e0601753592eb124b",
+    "url": "/AlexeySleptsov.github.io/static/css/2.b46dbf60.chunk.css"
+  },
+  {
+    "revision": "049b405db78b3b199202",
+    "url": "/AlexeySleptsov.github.io/static/css/main.59489dea.chunk.css"
+  },
+  {
+    "revision": "cd4e0601753592eb124b",
+    "url": "/AlexeySleptsov.github.io/static/js/2.93c11b71.chunk.js"
+  },
+  {
+    "revision": "049b405db78b3b199202",
+    "url": "/AlexeySleptsov.github.io/static/js/main.7f92984d.chunk.js"
+  },
+  {
+    "revision": "585d44bfb32c504e3158",
+    "url": "/AlexeySleptsov.github.io/static/js/runtime-main.7bcb2d17.js"
+  }
+]);
